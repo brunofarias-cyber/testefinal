067fcf5321b237435eca185ac439c143acf0d572
brunofarias-cyber <bruno.farias@prof.colegioperfil.com.br>
2025-12-24 10:05:23 -0300

    subir

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     build: {
         outDir: 'dist',
         sourcemap: false,
-        minify: 'terser',
+        minify: false,
         chunkSizeWarningLimit: 1000,
         rollupOptions: {
             output: {
